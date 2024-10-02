ee7366c2cd96378e41a378bc1110cf39275f58b6
yazilimciigenc <yazilimciigenc.112@gmail.com>
2024-10-02 09:30:55 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ bl_info = {
     'name': 'Multi Purpose',
     'author': 'Yazılımcı Genç',
     'description': "Bismillah! Blender'da işlerimizi kolaylaştırmak amacıyla yazılmıştır.",
-    'blender': (4, 0, 2),
-    'version': (1, 0, 6),
+    'blender': (4, 0, 0),
+    'version': (1, 0, 7),
     'location': 'View3D > Sidebar > Multi Purpose',
     'warning': '',
     'wiki_url': "",
@@ -22,8 +22,8 @@ from . import addon_updater_ops
 
 from .link_operations import MP_PT_LinkOperations, MP_OT_FindFilePaths, MP_OT_LibraryMake, MP_OT_RelationsMake
 #from .animation_operations import MP_PT_AnimationOperations, MP_OT_WalkingStraight, MP_OT_CreatePath, MP_OT_FollowPath, MP_OT_BreakPath, MP_OT_RootMove
-from .action_editor_set import MP_MT_DeleteActionsMenu, MP_OT_DeleteActionConfirm, MP_OT_DeleteAction, MP_OT_ActionEditorHeader, MP_OT_DeleteActionAssets
-from .scripting_set import MP_PT_Scripting_Settings, MP_MT_RunScript, MP_OT_ConfirmRunScript
+#from .action_editor_set import MP_MT_DeleteActionsMenu, MP_OT_DeleteActionConfirm, MP_OT_DeleteAction, MP_OT_ActionEditorHeader, MP_OT_DeleteActionAssets
+#from .scripting_set import MP_PT_Scripting_Settings, MP_MT_RunScript, MP_OT_ConfirmRunScript
 
     
 classes = (
@@ -35,11 +35,11 @@ classes = (
     #MP_OT_FollowPath, MP_OT_BreakPath, MP_OT_RootMove,
     
     # Action Editor Operations
-    MP_MT_DeleteActionsMenu, MP_OT_DeleteActionConfirm, MP_OT_DeleteAction, 
-    MP_OT_ActionEditorHeader, MP_OT_DeleteActionAssets,
+    #MP_MT_DeleteActionsMenu, MP_OT_DeleteActionConfirm, MP_OT_DeleteAction, 
+    #MP_OT_ActionEditorHeader, MP_OT_DeleteActionAssets,
     
     # Scripting Settings
-    MP_PT_Scripting_Settings, MP_MT_RunScript, MP_OT_ConfirmRunScript
+    #MP_PT_Scripting_Settings, MP_MT_RunScript, MP_OT_ConfirmRunScript
 )
 
 @addon_updater_ops.make_annotations
